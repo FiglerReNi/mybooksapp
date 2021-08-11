42bde82c3a0691b89e769e80d597db8c2db7ca99
figlerr <figler.renata@tmx.hu>
2021-08-11 11:49:33 +0200

    forras

---
+++ src/main/java/hu/tmx/mybooksapp/MybooksappApplication.java
@@ -0,0 +1,13 @@
+package hu.tmx.mybooksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybooksappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybooksappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/tmx/mybooksapp/MybooksappApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.tmx.mybooksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybooksappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
