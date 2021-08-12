cf69568613965c7c9a9fc798b868655c72535fce
figlerr <figler.renata@tmx.hu>
2021-08-12 15:32:54 +0200

    forras

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+    $('[name=booksButton]').click(function (){
+        window.open("/books", "_self")
+    });
+
+    $('[name=authorsButton]').click(function (){
+        window.open("/authors", "_self")
+    });
+
     $('[name=detailsButtonB]').click(function (){
         window.open("/books/"+ this.id, "_self")
     });
