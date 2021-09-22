557d14c3334a8dd5424aa74783dfe4dc1fe04668
figlerr <figler.renata@tmx.hu>
2021-09-22 14:43:03 +0200

    forras

--- src/main/java/hu/tmx/mybooksapp/repository/AuthorDao.java
+++ src/main/java/hu/tmx/mybooksapp/repository/AuthorDao.java
@@ -4,7 +4,24 @@ import hu.tmx.mybooksapp.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AuthorDao extends JpaRepository<Author, Integer>{
+
+    @Override
+    List<Author> findAll();
+
+    @Override
+    List<Author> findAllById(Iterable<Integer> iterable);
+
+    @Override
+    <S extends Author> S save(S s);
+
+    @Override
+    void delete(Author author);
+
+    @Override
+    void deleteAll();
 
 }
